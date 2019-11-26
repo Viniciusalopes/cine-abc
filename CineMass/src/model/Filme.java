a34a7f689391874f5bc741551df8e16d0b76d94c
Marcospaulo-paixao <contato.marcospaulo1@gmail.com>
2019-11-26 19:12:52 -0300

    model_filme

--- CineMass/src/model/Filme.java
+++ CineMass/src/model/Filme.java
@@ -14,6 +14,11 @@ public class Filme {
     private int id;
     private String titulo;
 
+    public Filme(int id, String titulo) {
+        this.id = id;
+        this.titulo = titulo;
+    }
+
     public int getId() {
         return id;
     }
